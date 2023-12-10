ea376721b11166cd1efd6a16abd4cee93752c3e3
ashrafshukurov <esrefsukurov7@gmail.com>
2023-12-10 19:37:13 +0400

    docker file,repo,models,liquibase is added

--- src/main/java/com/webApp/courseerpbackend/CourseErpBackendApplication.java
+++ src/main/java/com/webApp/courseerpbackend/CourseErpBackendApplication.java
@@ -1,13 +1,41 @@
 package com.webApp.courseerpbackend;
 
+import com.webApp.courseerpbackend.models.enums.user.UserStatus;
+import com.webApp.courseerpbackend.models.user.User;
+import com.webApp.courseerpbackend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseErpBackendApplication {
+public class CourseErpBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseErpBackendApplication.class, args);
 	}
 
+
+	private  UserRepository userRepository;
+
+	public CourseErpBackendApplication(UserRepository userRepository){
+		this.userRepository=userRepository;
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user=User.builder()
+				.email("ashraf.shukurow@gmail.com")
+				.name("ashraf")
+				.surname("Shukurow")
+				.status(UserStatus.ACTIVE)
+				.roleId(2L)
+				.password("ashraf2004")
+				.phoneNumber("0775231232")
+				.build();
+		userRepository.save(user);
+		System.out.println(String.format("%s id is inserted",user.getId()));
+	}
 }
--- src/main/java/com/webApp/courseerpbackend/controller/ControllerTest.java
+++ src/main/java/com/webApp/courseerpbackend/controller/ControllerTest.java
@@ -4,7 +4,7 @@ package com.webApp.courseerpbackend.controller;/*
  *
  */
 
-import com.webApp.courseerpbackend.models.base.BaseResponse;
+import com.webApp.courseerpbackend.dto.response.BaseResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
